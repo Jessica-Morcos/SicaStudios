43e10abb65c048d481a7438b8386a43c78c62a0f
Jessica Morcos <Jessicammorcos@gmail.com>
2025-10-15 01:56:12 -0400

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import SocialSidebar from "./components/SocialSidebar";
 import SocialSidebarLeft from "./components/SocialSidebarLeft";
