db976bfd41dc484f0018f82280454ee9e3270e97
Jessica Morcos <Jessicammorcos@gmail.com>
2025-10-16 17:56:03 -0400

    Hero Page Efficency Test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
 import { motion, useInView } from "framer-motion";
 import PolaroidModel from "./PolaroidModel";
-import PolaroidModelSmall from "./PolaroidModelSmall";
+
 
 export default function Hero() {
   const cameraPosition = [-26.331, 0.289, 48.012];
@@ -13,7 +13,7 @@ export default function Hero() {
   return (
     <div className="relative" ref={heroRef}>
       {/* 3D Canvas */}
-      <div className="absolute portrait:hidden w-full px-[5%] z-10" style={{ height: "200vh" }}>
+      <div className="absolute h-[200vh] portrait:h-[0vh] portrait:hidden w-full px-[5%] z-5">
         <Canvas
           shadows
           frameloop="always"
@@ -51,8 +51,8 @@ export default function Hero() {
       </div>
 
       {/* Hero Section */}
-      <section className="relative h-screen bg-transparent flex items-center justify-center overflow-hidden portrait:mr-[0rem] mr-[5rem]">
-        <div className="relative  z-10 left-[20rem] portrait:left-[0rem]  top-[-10%] portrait:top-[-30%]">
+      <section className="relative h-screen portrait:h-[75vh] bg-transparent flex items-center justify-center overflow-hidden portrait:mr-[0rem] mr-[5rem]">
+        <div className="relative  z-10 left-[20rem] portrait:left-[0rem]  top-[-10%] portrait:top-[-25%]">
           <h1 className="text-[2.8rem] portrait:text-[2rem]  font-semibold leading-[150%] tracking-[8px] text-center">
             Designer.
             <br />
@@ -63,42 +63,7 @@ export default function Hero() {
             solver.
           </h1>
         </div>
-        {/* <div className="absolute hidden portrait:block w-full  z-10" style={{ height: "100vh" }}>
-        <Canvas
-          shadows
-          frameloop="always"
-          dpr={[0.8, 2]}
-          gl={{ antialias: true, powerPreference: "high-performance" }}
-          performance={{ min: 0.6 }}
-          camera={{
-            position: cameraPosition,
-            fov: 45,
-            near: 0.1,
-            far: 1000,
-          }}
-        >
-  
-          <ambientLight intensity={2} />
-          <directionalLight
-            position={[3, 9, 2]}
-            intensity={1.4}
-            castShadow
-            shadow-mapSize={[512, 512]}
-          />
-    
-
-          <Suspense fallback={null}>
-            <PolaroidModelSmall
-              scale={[5, 5, 5]}
-              position={[0,-10, 0]}
-              rotation={[0.3, 0, 0]}
-              trigger={isInView}
-            />
-          </Suspense>
-
-          <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
-        </Canvas>
-      </div> */}
+       
       </section>
 
 
@@ -108,7 +73,7 @@ export default function Hero() {
         id="about"
         className="relative portrait:h-[50vh] h-[90vh] bg-transparent flex items-center justify-center overflow-hidden"
       >
-        <div className="relative portrait:mx-[1rem] top-[-15%] right-[15rem] portrait:right-[0rem] z-10 max-w-[25rem] flex flex-col landscape:items-start  ">
+        <div className="relative portrait:mx-[1rem] portrait:mt-[4rem] top-[-15%] right-[15rem] portrait:right-[0rem] z-30 max-w-[25rem] flex flex-col landscape:items-start  ">
           <div className="flex flex-col w-full">
             <h2 className="text-[2rem] font-bold tracking-[0.5rem] portrait:px-[1rem] ">
               About Me
